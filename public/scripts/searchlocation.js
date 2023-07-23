56ac61fa127ab58387df7445c2ee8b2c6747ee27
Michael Graves <29133471+michaelgrav@users.noreply.github.com>
2023-07-23 13:13:44 -0500

    Add prompt if search was wrong

--- public/scripts/searchlocation.js
+++ public/scripts/searchlocation.js
@@ -5,10 +5,18 @@ export function searchLocation(searchEntry) {
         fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + searchEntry + '&limit=' + "1" + '&appid=' + key)
         .then(function(resp) { return resp.json()  }) // Convert response to json
         .then(function(data) {
-            sessionStorage.setItem("searchLat", data[0].lat);
-            sessionStorage.setItem("searchLon", data[0].lon);
+            if(data.length == 0) {
+                window.alert("Search text was formatted invalid. Please do City, State");
+            } else {
+                sessionStorage.setItem("searchLat", data[0].lat);
+                sessionStorage.setItem("searchLon", data[0].lon);
+            }
+
+            return data;
         })
-        .then(function() {location.reload()})
+        .then(function(data) {
+            if(!data.length == 0) location.reload();
+        });
     } catch(error) {
         console.error(error)
     }
