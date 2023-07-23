5403e1ca844964e687addff7fc81bfc95eb611e6
Michael Graves <29133471+michaelgrav@users.noreply.github.com>
2023-07-23 18:41:08 -0500

    Put the hourly results in a table

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -119,6 +119,9 @@ function insertCurrentWeather(data) {
     container.innerHTML += content;
 }
 
+/*
+    These containers will hold each day's weather forecast
+*/
 function createFutureWeatherContainers(data) {
     const mainContainer = document.getElementById('futureWeatherContainer');
 
@@ -127,8 +130,27 @@ function createFutureWeatherContainers(data) {
     data.forEach(forecast => {
         var date = new Date(forecast.dt * 1000);
         var dateString = date.toDateString();
+        var tableBodyID = dateString + "-table-body";
+        var dayCardID = dateString + "-day-summary-card";
         const content = `
-            <div id="${dateString}"></div>
+            <div id="${dateString}">
+                <div id="${dayCardID}"></div>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Time</th>
+                                <th scope="col">Conditions</th>
+                                <th scope="col">Temp</th>
+                                <th scope="col">Feels Like</th>
+                                <th scope="col">Rain Chance</th>
+                                <th scope="col">Cloud Coverage</th>
+                            </tr>
+                        </thead>
+                        <tbody id="${tableBodyID}"></tbody>
+                    </table>
+                </div>
+            </div>
           `;
     
         mainContainer.innerHTML += content;
@@ -144,7 +166,7 @@ function insertFutureWeather(data, dataDaily) {
 
         // Actual current temp
         var fahrenheit = Math.round(((parseFloat(forecast.temp) - 273.15) * 1.8) + 32);
-        //var fahrenheitFeelLike = Math.round(((parseFloat(forecast.feels_like) - 273.15) * 1.8) + 32);
+        var fahrenheitFeelLike = Math.round(((parseFloat(forecast.feels_like) - 273.15) * 1.8) + 32);
 
         var date = new Date(forecast.dt * 1000);
         var dateString = date.toDateString();
@@ -156,7 +178,7 @@ function insertFutureWeather(data, dataDaily) {
 
         const tomorrow = new Date(new Date().getTime() + (24 * 60 * 60 * 1000));
 
-        const container = document.getElementById(dateString);
+        const container = document.getElementById(dateString + "-day-summary-card");
 
         // Date is today
         if(currDateString == dateString) {
@@ -215,7 +237,7 @@ function insertFutureWeather(data, dataDaily) {
             var emojiID = firstDigit(forecast.weather[0].id);
         }
 
-        const content = `
+        /*
             <div class="card">
             <h5 class="card-header text-dark">${dateText} at ${time} ${emojiMap.get(emojiID)}</h5>
             <div class="card-body text-dark">
@@ -225,9 +247,23 @@ function insertFutureWeather(data, dataDaily) {
             </div>
             </div>
             <br/>
+        */
+
+        const hourlyContainer = document.getElementById(dateString+"-table-body");
+
+
+        const content = `
+            <tr>
+                <td>${time}</td>
+                <td>${detailedForecastText}</td>
+                <td>${fahrenheit}&deg;</td>
+                <td>${fahrenheitFeelLike}</td>
+                <td>${rainChance}%</td>
+                <td>${forecast.clouds}%</td>
+            </tr>
           `;
     
-        container.innerHTML += content;
+        hourlyContainer.innerHTML += content;
     });
 
     // Insert the rest of the future days
